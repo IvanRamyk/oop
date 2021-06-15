5765a9bceb4caff42c462ebe29e73a7c1139066d
IvanRamyk <ivan.ramyk.official@gmail.com>
2021-06-15 23:59:44 +0300

    update local changes

--- semester6/lab1/backend/src/main/java/app/servlets/FlightServlet.java
+++ semester6/lab1/backend/src/main/java/app/servlets/FlightServlet.java
@@ -2,7 +2,9 @@ package app.servlets;
 
 
 import app.DAO.AirportDAO;
-import app.entities.Airport;
+import app.DAO.FlightDAO;
+import app.entities.Flight;
+import app.entities.FlightInfo;
 import com.google.gson.Gson;
 
 import javax.servlet.ServletException;
@@ -10,25 +12,21 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
-
 import java.util.List;
 
-@WebServlet(name = "AddServlet", urlPatterns = "/airports")
-
-public class AirportServlet extends HttpServlet {
-
+@WebServlet(name = "FlightServlet", urlPatterns = "/flights")
+public class FlightServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        List<Airport> airports = AirportDAO.getAllAirports();
+        List<Flight> flights = FlightDAO.getAllFlights();
         PrintWriter out = resp.getWriter();
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
-        String airportsJson = new Gson().toJson(airports);
-        out.print(airportsJson);
+        String flightsJson = new Gson().toJson(flights);
+        out.println(flightsJson);
         out.flush();
 
     }
@@ -46,9 +44,10 @@ public class AirportServlet extends HttpServlet {
         }
 
         Gson gson = new Gson();
-        Airport airport = gson.fromJson(jb.toString(), Airport.class);
-        AirportDAO.addAirport(airport);
+        FlightInfo flightInfo = gson.fromJson(jb.toString(), FlightInfo.class);
+        FlightDAO.addFlight(flightInfo);
     }
+
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -63,18 +62,19 @@ public class AirportServlet extends HttpServlet {
         }
 
         Gson gson = new Gson();
-        Airport airport = gson.fromJson(jb.toString(), Airport.class);
-        AirportDAO.updateAirport(airport);
+        FlightInfo flightInfo = gson.fromJson(jb.toString(), FlightInfo.class);
+        FlightDAO.updateFlight(flightInfo);
     }
+
 
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
         String id = req.getParameter("id");
         if (id != null) {
             int airportId = Integer.parseInt(id);
-            AirportDAO.deleteAirport(airportId);
+            FlightDAO.deleteFlight(airportId);  
         }
     }
 
 }
+
